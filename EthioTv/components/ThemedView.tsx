c69aa1c3eba77f1877ff82f46167d41a9c1aed25
Dagmawi Teka <dagtt316@gmail.com>
2024-12-26 16:35:15 +0300

    update to 52 template

--- EthioTv/components/ThemedView.tsx
+++ EthioTv/components/ThemedView.tsx
@@ -1,14 +1,20 @@
-import { View, type ViewProps } from 'react-native';
-
-import { useThemeColor } from '@/hooks/useThemeColor';
+import React from "react";
+import { View, ViewProps } from "react-native";
 
 export type ThemedViewProps = ViewProps & {
-  lightColor?: string;
-  darkColor?: string;
+  className?: string;
 };
 
-export function ThemedView({ style, lightColor, darkColor, ...otherProps }: ThemedViewProps) {
-  const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
-
-  return <View style={[{ backgroundColor }, style]} {...otherProps} />;
+export function ThemedView({
+  style,
+  className,
+  ...otherProps
+}: ThemedViewProps) {
+  return (
+    <View
+      className={`bg-background dark:bg-background-dark ${className || ""}`}
+      style={style}
+      {...otherProps}
+    />
+  );
 }
--- EthioTv/constants/Colors.ts
+++ EthioTv/constants/Colors.ts
@@ -1,26 +1,16 @@
-/**
- * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
- * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
- */
-
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
-
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
+    text: "rgb(17 24 28)",
+    background: "rgb(255 255 255)",
+    tint: "rgb(10 126 164)",
+    icon: "rgb(104 112 118)",
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
+    text: "rgb(236 237 238)",
+    background: "rgb(21 23 24)",
+    tint: "rgb(255 255 255)",
+    icon: "rgb(155 161 166)",
   },
-};
+} as const;
+
+export type ColorName = keyof typeof Colors.light & keyof typeof Colors.dark;
