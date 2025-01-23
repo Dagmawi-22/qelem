799b995bee121ba75613da3909a6d1afcfe8a984
Dagmawi Teka <dagtt316@gmail.com>
2025-01-23 11:28:25 +0300

    use grade for pdf listing & filtering

--- constants/Pdfs.ts
+++ constants/Pdfs.ts
@@ -1,6 +1,6 @@
 interface Section {
   title: string;
-  subPdfs: { title: string; path: string; cover?: string; imgUrl?: string }[];
+  subPdfs: { title: string; path: string; grade?: number; imgUrl?: string }[];
 }
 
 export const PDF_SECTIONS: Section[] = [
@@ -10,84 +10,84 @@ export const PDF_SECTIONS: Section[] = [
       {
         title: "Agriculture",
         path: "https://drive.google.com/file/d/1NN_6sZtIoxiGGMWyJcy1sHhdsSVqJ319/view?usp=sharing",
-        cover: require("../assets/covers/12/ict.png"),
+        grade: 12,
         imgUrl:
           "https://www.vectorstock.com/royalty-free-vector/magical-open-book-engraving-vector-52016463",
       },
       {
         title: "Amharic",
         path: "https://drive.google.com/file/d/1A7cJDK2A_FrF6dJ0XvG2IA2HGhLSvPna/view?usp=sharing",
-        cover: require("../assets/covers/12/agri.png"),
+        grade: 12,
         imgUrl:
           "https://www.vectorstock.com/royalty-free-vector/magical-open-book-engraving-vector-52016463",
       },
       {
         title: "Biology",
         path: "https://drive.google.com/file/d/11O3rln-_932Z8Rm7DkR1Dmebi-zxnu_u/view?usp=sharing",
-        cover: require("../assets/covers/12/agri.png"),
+        grade: 12,
         imgUrl:
           "https://www.vectorstock.com/royalty-free-vector/magical-open-book-engraving-vector-52016463",
       },
       {
         title: "Chemistry",
         path: "https://drive.google.com/file/d/1eAIn3UOOkwlWUQlna1W1xIRetbIlMF83/view?usp=sharing",
-        cover: require("../assets/covers/12/agri.png"),
+        grade: 12,
         imgUrl:
           "https://www.vectorstock.com/royalty-free-vector/magical-open-book-engraving-vector-52016463",
       },
       {
         title: "Civics",
         path: "https://drive.google.com/file/d/1sPFzedB67nU1YWohMB9zwQNWoTWSA_GD/view?usp=sharing",
-        cover: require("../assets/covers/12/agri.png"),
+        grade: 12,
         imgUrl:
           "https://www.vectorstock.com/royalty-free-vector/magical-open-book-engraving-vector-52016463",
       },
       {
         title: "Economics",
         path: "https://drive.google.com/file/d/194uT9P1uB5xbt6gCcGj23eb5Q7VtvKp5/view?usp=sharing",
-        cover: require("../assets/covers/12/agri.png"),
+        grade: 12,
         imgUrl:
           "https://www.vectorstock.com/royalty-free-vector/magical-open-book-engraving-vector-52016463",
       },
       {
         title: "English",
         path: "https://drive.google.com/file/d/1nKTgxw1R7QZ4P9mOMJkcbI3D5sxt4yqp/view?usp=sharing",
-        cover: require("../assets/covers/12/agri.png"),
+        grade: 12,
         imgUrl:
           "https://www.vectorstock.com/royalty-free-vector/magical-open-book-engraving-vector-52016463",
       },
       {
         title: "General Business",
         path: "https://drive.google.com/file/d/1c0V0ykEWFXizoqDU1_e5meYaA4gzIXUk/view?usp=sharing",
-        cover: require("../assets/covers/12/agri.png"),
+        grade: 12,
         imgUrl:
           "https://www.vectorstock.com/royalty-free-vector/magical-open-book-engraving-vector-52016463",
       },
       {
         title: "Geography",
         path: "https://drive.google.com/file/d/1Yd5xtN90GPDfpz0R0EUGCLbn5Wz8BnEK/view?usp=sharing",
-        cover: require("../assets/covers/12/agri.png"),
+        grade: 12,
         imgUrl:
           "https://www.vectorstock.com/royalty-free-vector/magical-open-book-engraving-vector-52016463",
       },
       {
         title: "History",
         path: "https://drive.google.com/file/d/1tkkIIjwB6N04EGPeQsEd-GCJYG3duW9x/view?usp=sharing",
-        cover: require("../assets/covers/12/agri.png"),
+        grade: 12,
         imgUrl:
           "https://www.vectorstock.com/royalty-free-vector/magical-open-book-engraving-vector-52016463",
       },
       {
         title: "ICT",
         path: "https://drive.google.com/file/d/13-QpjTkX3gGE-_WBkB94ZOcStbvqqv64/view?usp=sharing",
-        cover: require("../assets/covers/12/agri.png"),
+        grade: 12,
         imgUrl:
           "https://www.vectorstock.com/royalty-free-vector/magical-open-book-engraving-vector-52016463",
       },
       {
         title: "Physics",
         path: "https://drive.google.com/file/d/1W0YTbmtJNlMX-fWAjnhGEZrzXdUR-gSA/view?usp=sharing",
-        cover: require("../assets/covers/12/agri.png"),
+        grade: 12,
         imgUrl: "",
       },
     ],
